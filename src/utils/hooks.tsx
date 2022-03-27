12c725679b774dbd6315090b4ec161bfce9770ad
Michael Coloma Calva <copyfast_team@gmx.com>
2022-03-27 23:36:51 +0200

    added hook to listener resize window

---
+++ src/utils/hooks.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+//Return the size of width and height of screen
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
